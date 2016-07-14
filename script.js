d55a7abc2dba44dec1f2c689daf6f2c5b946741f
will <willsmith12289@gmail.com>
2016-07-14 05:08:29 -0400

    markers

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 (function(window, mapster) {
-	//map options
-	var options = mapster.MAP_OPTIONS,
-	element = document.getElementById('map-canvas'),
-	//map
 
-	map = Mapster.create(element, options);
-	map.zoom(18);
-	map._on('click', function (e) {
-		alert('clicked');
-		console.log(e);
-		console.log(this);
-	});
-	map.addMarker({
-		lat: 40.5790250,
-		lng: -74.9203500,
-		draggable: true,
-		visible: true,
-		id: 1,
-		icon: 'https://mapicons.mapsmarker.com/wp-content/uploads/mapicons/shape-default/color-9d7050/shapecolor-color/shadow-1/border-dark/symbolstyle-white/symbolshadowstyle-dark/gradient-no/waterway-canal-icon.png'
-	});
+  // map options
+  var options = mapster.MAP_OPTIONS,
+  element = document.getElementById('map-canvas'),
+  // map
+  map = mapster.create(element, options);
 
+  map.zoom(17);
 
-}(window, window.Mapster || (window.Mapster = {})));
+  var marker = map.addMarker({
+    lat: 40.5790250,
+    lng: -74.9203500,
+    draggable: true,
+    content: '<div style="color: #F00;"> I like Food </div>',
+    icon: 'mapicons/drink.png'
+  });
+
+    var marker2 = map.addMarker({
+    lat: 40.5790250,
+    lng: -74.9203500,
+    draggable: true,
+    content: 'I like rice',
+    icon: 'mapicons/farmstand.png'
+  });
+
+  
+}(window, window.Mapster));
